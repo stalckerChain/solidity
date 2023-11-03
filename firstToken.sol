6e5b7f6b0ba8ce5e948e79ba011cafdc00f8980a
stalckerCryptofamily <149796345+stalckerCryptofamily@users.noreply.github.com>
2023-11-03 19:01:50 +0600

    Update firstToken.sol

--- firstToken.sol
+++ firstToken.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.6.0;
 import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.1.0/contracts/token/ERC20/ERC20.sol';
 
 // This ERC-20 contract mints the specified amount of tokens to the contract creator.
+// 123123123
 contract MyFirstToken is ERC20 {
   constructor(uint256 initialSupply) ERC20("MyFirstToken", "HEDGE") public {
     _mint(msg.sender, initialSupply);
