a94eeebace877c1bf7593970631fe8b4c9586df4
stalckerChain <121632159+stalckerChain@users.noreply.github.com>
2023-11-24 19:16:10 +0600

    Update Marketplace.sol

--- Marketplace.sol
+++ Marketplace.sol
@@ -1,4 +1,8 @@
-pragma solidity >0.7.0 <0.8.0;
+// SPDX-License-Identifier: GPL-3.0
+
+// pragma solidity >0.7.0 <0.8.0;
+pragma solidity >=0.8.2 <0.9.0;
+
 
 contract Marketplace {
     event PurchasedItem(address buyer, uint price);
@@ -6,5 +10,17 @@ contract Marketplace {
     function buy() public {
         // ...
         emit PurchasedItem(msg.sender, msg.value);
+    }
+
+      modifier onlySeller() {
+        require(
+            msg.sender == seller,
+            "Only seller can put an item up for sale."
+        );
+        _;
+    }
+
+    function listItem(seller) public view onlySeller {
+        // ...
     }
 }
